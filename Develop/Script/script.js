6807ca87052acf3498e9cc6cae6997a627d22606
Taylor Gonzales <hello@taylorgonz.com>
2021-01-23 21:40:44 -0500

    color changing background set

--- Develop/Script/script.js
+++ Develop/Script/script.js
@@ -1,4 +1,32 @@
+const rows = $("div.row");
+let currentHour = parseInt(dayjs().format("H"));
+const currentDay = dayjs().format(`MMM, D`);
+// displaying current date in header
+$("#currentDay").text(currentDay);
 
-const currentTime = dayjs().format(`MMM, D`);
 
-$('#currentDay').text(currentTime);
+
+// setting background color according to time
+Array.from(rows).forEach((row) => {
+    // 
+  let rowHour = parseInt(row.id);
+
+  console.log(rowHour)
+
+  if (rowHour) {
+    // compare row id to current hour and sets color
+    if (currentHour === rowHour) {
+      setColor(row, "#ff6961");
+    } else if (currentHour < rowHour) {
+      setColor(row, "#77dd77");
+    } else{
+      setColor(row, "#d3d3d3");
+    }
+  }
+});
+function setColor(element, color) {
+  element.style.backgroundColor = color;
+}
+console.log(currentHour)
+console.log(rows.id);
+console.log(rows);
