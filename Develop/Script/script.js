0f95f4a4255cadedf8a30f3a817ced95826f7845
Taylor Gonzales <hello@taylorgonz.com>
2021-01-24 21:08:24 -0500

    set local storage function working

--- Develop/Script/script.js
+++ Develop/Script/script.js
@@ -1,17 +1,53 @@
 const rows = $("div.row");
-let currentHour = parseInt(dayjs().format("H"));
+const currentHour = parseInt(dayjs().format("H"));
 const currentDay = dayjs().format(`MMM, D`);
+let storedPlans = [];
+const storedPlansName = "schedule plans"
+const descriptionInput = $(".description");
+console.log(storedPlans)
 // displaying current date in header
 $("#currentDay").text(currentDay);
 
+// save input function
+function setPlans(uText, uId) {
+    storedBlock= {
+    id : uId,
+    input : uText.trim()
+}
+    for (let i = 0; i < storedPlans.length; i++) {
+            if (storedBlock.id === storedPlans[i].id) {
 
+                storedPlans.splice(i, 1);
 
+                localStorage.setItem(storedPlansName, JSON.stringify(storedPlans));
+
+                return null;
+            }
+        }
+    storedPlans.push(storedBlock);
+        localStorage.setItem(storedPlansName, JSON.stringify(storedPlans))
+}
+
+// get stored plans from localStorage
+
+function getPlans() {
+   storedPlans.forEach(function($))
+}
+
+// creating storage function
+$('.saveBtn').on('click', function(event) {
+    event.preventDefault();
+    let userInput = $(this).siblings(".description").val();
+    let blockId = $(this).siblings(".description").attr("id");
+
+    setPlans(userInput, blockId);
+});
+console.log(localStorage)
 // setting background color according to time
 Array.from(rows).forEach((row) => {
     // 
   let rowHour = parseInt(row.id);
 
-  console.log(rowHour)
 
   if (rowHour) {
     // compare row id to current hour and sets color
@@ -27,6 +63,3 @@ Array.from(rows).forEach((row) => {
 function setColor(element, color) {
   element.style.backgroundColor = color;
 }
-console.log(currentHour)
-console.log(rows.id);
-console.log(rows);
