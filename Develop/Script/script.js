9ccbd839b47c249efd565b2277bb8362566df6a6
Taylor Gonzales <hello@taylorgonz.com>
2021-01-24 23:40:49 -0500

    finishing up

--- Develop/Script/script.js
+++ Develop/Script/script.js
@@ -1,53 +1,54 @@
 const rows = $("div.row");
+const displayArea = $("textarea");
 const currentHour = parseInt(dayjs().format("H"));
 const currentDay = dayjs().format(`MMM, D`);
 let storedPlans = [];
-const storedPlansName = "schedule plans"
 const descriptionInput = $(".description");
-console.log(storedPlans)
 // displaying current date in header
 $("#currentDay").text(currentDay);
 
-// save input function
-function setPlans(uText, uId) {
-    storedBlock= {
-    id : uId,
-    input : uText.trim()
-}
-    for (let i = 0; i < storedPlans.length; i++) {
-            if (storedBlock.id === storedPlans[i].id) {
-
-                storedPlans.splice(i, 1);
-
-                localStorage.setItem(storedPlansName, JSON.stringify(storedPlans));
-
-                return null;
-            }
-        }
-    storedPlans.push(storedBlock);
-        localStorage.setItem(storedPlansName, JSON.stringify(storedPlans))
-}
-
 // get stored plans from localStorage
-
 function getPlans() {
-   storedPlans.forEach(function($))
+    let storedAppointments= JSON.parse(localStorage.getItem("appointments"));
+    console.log(storedAppointments);
+
+    if (storedAppointments !== null) {
+        for (i = 0; i < storedAppointments.length; i++) {
+            $('#' + storedAppointments[i].id).val(storedAppointments[i].input);
+        };
+    }
 }
+getPlans();
 
 // creating storage function
-$('.saveBtn').on('click', function(event) {
-    event.preventDefault();
-    let userInput = $(this).siblings(".description").val();
-    let blockId = $(this).siblings(".description").attr("id");
+$(".saveBtn").on("click", function (event) {
+    // preventing bubbling
+  event.preventDefault();
+//   variable for storing input info inside an array
+  let userInput = $(this).siblings(".description").val();
+  let blockId = $(this).siblings(".description").attr("id");
 
-    setPlans(userInput, blockId);
+  storedBlock = {
+    id: blockId,
+    input: userInput,
+  };
+
+  if (storedPlans === null) {
+    localStorage.setItem(
+      "appointments",
+      JSON.stringify([{ id: blockId, input: userInput }])
+    );
+  } else {
+    storedPlans.push(storedBlock);
+    localStorage.setItem("appointments", JSON.stringify(storedPlans));
+  }
 });
-console.log(localStorage)
+
+console.log(localStorage);
 // setting background color according to time
 Array.from(rows).forEach((row) => {
-    // 
+  //
   let rowHour = parseInt(row.id);
-
 
   if (rowHour) {
     // compare row id to current hour and sets color
@@ -55,11 +56,18 @@ Array.from(rows).forEach((row) => {
       setColor(row, "#ff6961");
     } else if (currentHour < rowHour) {
       setColor(row, "#77dd77");
-    } else{
+    } else {
       setColor(row, "#d3d3d3");
     }
   }
 });
+// function for setting color
 function setColor(element, color) {
   element.style.backgroundColor = color;
 }
+
+$('.clear').on('click', function(event){
+    event.preventDefault();
+    localStorage.clear();
+    $('textarea').val('');
+});
--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # Day-Planner
+
+## Ready to schedule your day!? 
+
+This is an easy to use day planner for all you 8am-5pm scheduling needs! The planner indicates wether your event is the in the future(green background), present(red background), or it's already in the past(grey background). 
+
+Below I have a screenshots of the application.
+![alt text](Assets/scheduler.png)
+
+## Instructions
+
+- Click on the input area and type in any appointment/plans!
+- Click on the save icon on the right and your input will be saved to your local storage, so don't worry if you accidentally hit the refresh button, your info will still be there!
+- click the clear all button to reset your page!
+
+
+
+## Usage
+This is a simple responsive sight and has
+ - css
+ - html
+ - bootstrap
+ - javascript
+ - day.js
+
+## Credits
+Credit to [Mike Fearnley](https://michaelfearnley.com/) and [Scott Byers](https://github.com/switch120)
+
+and the UNH Fullstack Coding Bootcamp
+
+## Links 
+Here is the link to the deployed site: https://taylorgonz.github.io/day-planner/
+## Contact
+
+if you want to contact me for any more questions here are my links!
+
+hello@taylorgonz.com
+<br>
+[Website](http://www.taylorgonz.com)
+
+## Licensing
+ [General Public License](https://opensource.org/licenses/GPL-2.0)
